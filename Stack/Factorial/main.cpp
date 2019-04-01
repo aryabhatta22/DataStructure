b0f75cc51eb5546f9cb8859b54c7ea08175d9ae8
Tarun Sharma <tarun.hive@hotmail.com>
2019-04-01 15:14:14 +0530

    new files to stack added

---
+++ Stack/Factorial/main.cpp
@@ -0,0 +1,47 @@
+/***********************************
+ * Description :
+ * Date : 
+ **********************************/
+
+
+#include <iostream>
+
+using namespace std;
+
+class stack
+{
+    int arr[1024], ptr;
+ public:
+    stack()
+     {
+      ptr = 0;
+     }
+     void push(int n)
+     {
+      arr[ptr++]=n;
+     }
+     int pop()
+     {
+      return arr[--ptr];
+     }
+};
+
+int main()
+{
+     int n, i;
+     stack s;
+     
+     cin>>n;
+     for(i=n;i>0;i--)
+     {
+      s.push(i);
+     }
+     
+     int fact = 1;
+     
+     while(n--)
+         fact*=s.pop();
+         
+     cout<<"Factorial: "<<fact<<endl;
+     return 0;
+}
